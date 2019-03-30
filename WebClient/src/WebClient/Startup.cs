66d26d4b97a3ca434a602bc4774b41258ad6f925
Maciej <mbubelski@gmail.com>
2019-03-30 18:47:17 +0100

    WebClient host locally in IIS

--- WebClient/src/WebClient/Startup.cs
+++ WebClient/src/WebClient/Startup.cs
@@ -13,6 +13,7 @@ namespace WebClient
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<IISServerOptions>(o => { o.AutomaticAuthentication = false; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
